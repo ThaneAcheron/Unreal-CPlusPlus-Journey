a964c25a2cf1d8d7705902ecd532bad970c369ec
Craig Turner <craig.turner944@gmail.com>
2018-12-10 19:59:05 +0200

    fx

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -4,7 +4,7 @@
 #include "GameFramework/PlayerController.h"
 #include "Engine/World.h"
 #include "GameFramework/Actor.h"
-
+#include "Engine/Public/DrawDebugHelpers.h"
 #define OUT
 
 // Sets default values for this component's properties
@@ -40,10 +40,24 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPoint, OUT PlayerViewPoinRotation);
 
+	FVector LineTraceEnd =  PlayerViewPoint + PlayerViewPoinRotation.Vector()  * reach;
+
 	UE_LOG(LogTemp, Warning, TEXT("Location: ViewPoint %s || Rotation: Rotation %s"), *PlayerViewPoint.ToString(), *PlayerViewPoinRotation.ToString());
 
-	//Raycast to reach distance
+	//Debug
+	DrawDebugLine(GetWorld(), PlayerViewPoint, LineTraceEnd, FColor(255,0,0), false, 0.0f, 0, 10.f);
+	
+	//LineTrace Raycast
+	FHitResult LineTraceHit; 
 
-	//See what we hit
+	FCollisionQueryParams TraceParam(FName(TEXT("")), false, GetOwner());
+	//SetUp Query param
+	GetWorld()->LineTraceSingleByObjectType(OUT LineTraceHit, PlayerViewPoint, LineTraceEnd, FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody), TraceParam);
+
+	AActor *hitActor = LineTraceHit.GetActor();
+	if (hitActor)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("You hit %s"), *hitActor->GetName());
+	}
 }
 
